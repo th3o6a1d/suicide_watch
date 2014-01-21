3d9f248a4b72dc141600f65a69f8c9fbb4bf4021
Jason Theobald <jtheo540@gmail.com>
2014-01-20 23:08:48 -0500

    fixed some things

--- stream.py
+++ stream.py
@@ -1,11 +1,12 @@
-from TwitterAPI import TwitterAPI, TwitterRestPager
+# coding: utf-8
+from TwitterAPI import TwitterAPI
 import json 
 import httplib
 import sqlite3 
 import sys
 
-### Credentials are saved in .json configuration file
-a = open('../credentials.json','r')
+### Credentials are saved in .json configuration file.  Obtain your credentials from dev.twitter.com.  
+a = open('credentials.json','r')
 f = json.load(a)
 
 ### Initialize the API
@@ -20,7 +21,7 @@ cur = con.cursor()
 stream = api.request('statuses/filter',{'language':'en','locations':'-74,40,-73,41'})
  
 ### Wordlist is from Argyle, Trenton et al. "Tracking Suicide Risk Factors Through Twitter in the US."  Crisis 2014; Vol. 35(1):51–59.
-wordlist = ["Me abused depressed", "me hurt depressed", "feel hopeless depressed", "feel alone depressed", "I feel helpless", "I feel worthless", "I feel sad", "I feel empty", "I feel anxious", "sleeping &quot;a lot&quot; lately", "I feel irritable", "I feel restless", "depressed alcohol", "sertraline", "zoloft", "prozac", "pills depressed", "suicide once more", "me abused suicide", "pain suicide", "I&#039;ve tried suicide before", "mom suicide tried", "sister suicide tried", "brother suicide tried", "friend suicide", "suicide attempted sister", "suicide thought about before", "thought suicide before", "had thoughts suicide", "had thoughts killing myself", "used thoughts suicide", "once thought suicide", "past thoughts suicide", "multiple thought suicide", "stop cutting myself", "I&#039;m being bullied", "I&#039;ve been cyber bullied", "feel bullied I&#039;m", "stop bullying me", "keeps bullying me", "always getting bullied", "gun suicide", "shooting range went", "gun range my", "I was diagnosed schizophrenia", "been diagnosed anorexia", "diagnosed bulimia", "I diagnosed OCD", "I diagnosed bipolar", "I diagnosed PTSD", "diagnosed borderline personality disorder", "diagnosed panic disorder", "diagnosed social anxiety disorder", "dad fight again", "parents fight again", "I impulsive"]
+wordlist = ["Me abused depressed", "me hurt depressed", "feel hopeless depressed", "feel alone depressed", "I feel helpless", "I feel worthless", "I feel sad", "I feel empty", "I feel anxious", "sleeping \"a lot\" lately", "I feel irritable", "I feel restless", "depressed alcohol", "sertraline", "zoloft", "prozac", "pills depressed", "suicide once more", "me abused suicide", "pain suicide", "I\'ve tried suicide before", "mom suicide tried", "sister suicide tried", "brother suicide tried", "friend suicide", "suicide attempted sister", "suicide thought about before", "thought suicide before", "had thoughts suicide", "had thoughts killing myself", "used thoughts suicide", "once thought suicide", "past thoughts suicide", "multiple thought suicide", "stop cutting myself", "I\'m being bullied", "I\'ve been cyber bullied", "feel bullied I\'m", "stop bullying me", "keeps bullying me", "always getting bullied", "gun suicide", "shooting range went", "gun range my", "I was diagnosed schizophrenia", "been diagnosed anorexia", "diagnosed bulimia", "I diagnosed OCD", "I diagnosed bipolar", "I diagnosed PTSD", "diagnosed borderline personality disorder", "diagnosed panic disorder", "diagnosed social anxiety disorder", "dad fight again", "parents fight again", "I impulsive"]
 
 ### Looks for redflag phrases and dumps them in db and txt format
 for tweet in stream.get_iterator():
